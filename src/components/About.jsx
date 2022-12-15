10a0ee6bff364ec7597e3f1b7d7abc756d8f48b9
Rossini Fonseca <rossinidrum@outlook.com>
2022-12-14 22:55:21 -0300

    fix about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,16 +17,18 @@ const About = () => {
       <article className="w-full h-auto  lg:w-1/2 flex flex-col justify-center  items-center lg:items-start ">
         <h1 className="text-secondary text-xl lg:text-3xl mt-10">SOBRE MIM</h1>
         <p className="font-light text-secondary-white lg:w-4/5 text-center lg:text-justify">
-          Meu nome é Rossini Fonseca e atuo com desenvolvimento de software a
-          mais de 1 e meio. Tenho como objetivo desenvolver soluções técnicas,
-          tornando problemas em soluções, corrindo erros e proporcionando sempre
-          melhorias em sistemas existentes. Possuo expêriencia no
-          desenvolvimento de aplicações com React, onde utilizo das melhores
-          ferramentas do Front-end para criações de interfaces de usuários.tenho
-          conheciment no desenvolvimento banckend com Nodejs, banco de dados
-          relacionais e não realacionas (mysql, mongodb), aplicações dastécnicas
-          de clean code, prática nas implemetações de teste uniários. e
-          versionamento git.
+          Olá, sou desenvolvedor de software com foco em desenvolvimento web com
+          mais de 1 ano e 6 meses na área, sendo 1 ano atuando como
+          desenvolvedor frontend em projetos freelancers e me dedicando a 6
+          meses como desenvolvedor backend. Tenho como objetivo desenvolver
+          soluções técnicas, tornando problemas em soluções, corrigindo erros e
+          proporcionando sempre melhorias em sistemas existentes. Possuo
+          expêriencia no desenvolvimento de aplicações com React, onde utilizo
+          das melhores ferramentas do frontend para criações de interfaces de
+          usuário, possuo conhecimento no desenvolvimento banckend com Nodejs,
+          banco de dados relacionais e não relacionas (mysql, mongodb),
+          conhecimento em práticas de clean code e implementação de teste
+          uniários. Entre em contato comigo para me conhecer um pouco mais.
         </p>
         <button
           type="button"
